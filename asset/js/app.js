d3077781c47c42318d30a07b2bf3a3473d8caac6
MaxenceBossin <maxbossin25@gmail.com>
2022-03-09 00:16:05 +0100

    css fonctionnel

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,34 +1,30 @@
 const url = 'https://jsonplaceholder.typicode.com/posts'
+// balise de toute l'app 
+let app = document.querySelector('#app')
+// console.log(app);
 const header = document.querySelector('.header')
-console.log(header);
-const content = document.querySelector('.list')
+
 const form = document.querySelector('form')
 
 const titleInput = document.querySelector('#titleInput')
 const bodyInput = document.querySelector('#bodyInput')
-
-function test(){console.log('est');}
-
-
-    headerHtml = `
-    <div class="test">
+const headerHtml = `
     <div class="header">
-    <h1>Exos sur les API</h1>
-        <h2>Ajout nouvelle API</h2>
-            <div class="add">
-                <form method='post 'action="">
-                    <label for="titleInput">Titre</label>
-                    <input name='titleInput' id="titleInput" type="text">
-                    <label for="bodyInput">Corp</label>
-                    <input name='bodyInput' id="bodyInput" type="text">
-                    <input id='envoyer' value="envoyer" type="submit">
-                </form>            
+        <h1>Exos sur les API</h1>
+            <h2>Ajout nouvelle API</h2>
+                <div class="add">
+                    <form method='post 'action="">
+                        <label for="titleInput">Titre* :</label>
+                        <input name='titleInput' id="titleInput" type="text" placeholder='rentrez titre ici'>
+                        <label for="bodyInput">Corp* :</label>
+                        <input name='bodyInput' id="bodyInput" type="text" placeholder='message...'>
+                        <div class="btnInput"></input><input id='envoyer' value="envoyer" type="submit">
+                    </form>            
+                </div>
     </div>
-    </div>
-    </div>
-    `
+`
 
-console.log(headerHtml)
+// console.log(headerHtml)
 function home()
 {
     fetch(url)
@@ -36,51 +32,47 @@ function home()
         return response.json();
     })
     .then(function(myJson) {
-        // console.log('reponse ok');
-        // console.log(myJson);
-        
-        console.log(headerHtml)
-        let html
+        let html ='';
         for(let element of myJson){
-            // console.log(element);
             html += `
-            <h2> ${element.title}</h2>
-            <p> ${element.body}</p>
-            <button class="plus" id="${element.id}">En savoir plus !</button>
+            <div class="content">
+                <h2> ${element.title}</h2>
+                <p> ${element.body}</p>
+                <button class="plus" id="${element.id}">En savoir plus !</button>
+            </div>
             `
         } 
-        document.body.innerHTML +=  headerHtml      
-        const test = document.querySelector('.test') 
-        
-        test.innerHTML += html
-
+        // ajout du header dans l'app (balise html)
+        app.innerHTML +=  headerHtml
+        // ajout du retour de l'api        
+        app.innerHTML += html
+        // sendDateOld()
         const btn = document.querySelectorAll('.plus')
         return btn
     }).then(function(btn){
-        // showDetail(btn);
+        showDetail(btn);
     })
 };
 // seconde page
 // on renvoit la liste de bouton, le foreach permet de detecter l'id
 // l'id permet d'appeler l'api avec le bon post
-
 function showDetail(btn)
 {
     btn.forEach(element => {
         element.addEventListener('click',() => {
-            console.log(element.id);
+            // console.log(element.id);
             let id = element.id;
-            document.body.innerHTML = ''
+            app.innerHTML = '<div id="app"></div>'
             // construire le nouveau 
             let urlPost = 'https://jsonplaceholder.typicode.com/posts/'+id
             fetch(urlPost)
             .then(function(response) {
-                console.log('ok');
+                // console.log('ok');
                 return response.json();
             })
             .then(function(myJsonSpe){
-                console.log('ok reponse');
-                console.log(myJsonSpe);
+                // console.log('ok reponse');
+                // console.log(myJsonSpe);
                 let html = `
                 <div id="${myJsonSpe.id}" class="article">
                 <h2> ${myJsonSpe.title}</h2>
@@ -89,17 +81,16 @@ function showDetail(btn)
                 </div> 
                 <h3> Commentaires de l'article</h3>                  
                 `
-                document.body.innerHTML = html
+                app.innerHTML += html
                 let urlComment = 'https://jsonplaceholder.typicode.com/comments'
                 fetch(url)
                 .then(function(response) {
                     return response.json();
                 })
                 .then(function(comments){
-                    const article = document.querySelector('.article')
+                    const article = document.querySelector('.article')                  
                     
-                    
-                    console.log(comments)
+                    // console.log(comments)
                     let html =''
                     comments.forEach(element => {
                         if(article.id == element.userId){
@@ -109,12 +100,23 @@ function showDetail(btn)
                             `
                         }
                     });
-                    document.body.innerHTML += html
+                    app.innerHTML += html
                     const btnreturn = document.querySelector('#return')
-                    console.log(btnreturn);
+                    // console.log(btnreturn);
                     btnreturn.addEventListener('click', function () { 
-                        document.body.innerHTML = '';
-                        console.log('test');
+                        console.log(app);
+                        app = document.querySelector('#app')
+                        app.innerHTML = `
+                        <div id="app">
+                            <div class="content">
+                                <h2>Liste de tout les posts</h2>
+                                    <!-- ajout en js -->
+                            </div>
+                        </div>
+                        `
+                        // 
+                        console.log(app);
+                        console.log('appel de la fonction home');
                         home()
                     })
                 })
@@ -123,9 +125,9 @@ function showDetail(btn)
     })       
 }    
 
-home();
+
 // envoie data
-form.addEventListener('submit', e => {
+function sendDateOld() {form.addEventListener('submit', e => {
     e.preventDefault()
 
     fetch('https://jsonplaceholder.typicode.com/posts', {
@@ -143,3 +145,8 @@ form.addEventListener('submit', e => {
   .then((json) => console.log(json));
 
 });
+}
+
+
+
+home();
